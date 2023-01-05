f99d8196c0e4209080202b876c60838d2fe37436
Kaushal-Vasava <kaushal.vasava@algozen.in>
2023-01-05 15:17:28 +0530

    Add Animaztion and change light and dark themes

--- app/src/main/java/com/lahsuak/apps/jetpackcomposebasic/MainActivity.kt
+++ app/src/main/java/com/lahsuak/apps/jetpackcomposebasic/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.lahsuak.apps.jetpackcomposebasic
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -11,6 +13,7 @@ import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.lahsuak.apps.jetpackcomposebasic.ui.theme.JetPackComposeBasicTheme
@@ -25,7 +28,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            JetPackComposeBasicTheme {
+            JetPackComposeBasicTheme(dynamicColor = false) {
                 // A surface container using the 'background' color from the theme
                 MyApp(modifier = Modifier.fillMaxSize())
             }
@@ -36,9 +39,9 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun MyApp(modifier: Modifier = Modifier) {
     /** PERSISTENT STATE
-    * Instead of using remember you can use rememberSaveable.
+     * Instead of using remember you can use rememberSaveable.
     This will save each state surviving configuration changes (such as rotations) and process death.
-    **/
+     **/
     var shouldShowOnboarding by rememberSaveable { mutableStateOf(true) }
 
     Surface(modifier) {
@@ -54,7 +57,13 @@ fun MyApp(modifier: Modifier = Modifier) {
 fun Greeting(name: String) {
     var expanded by rememberSaveable { mutableStateOf(false) }
 
-    val extraPadding = if (expanded) 48.dp else 0.dp
+    val extraPadding by animateDpAsState(
+        if (expanded) 48.dp else 0.dp,
+        animationSpec = spring(
+            dampingRatio = Spring.DampingRatioMediumBouncy,
+            stiffness = Spring.StiffnessLow
+        )
+    )
 
     Surface(
         color = MaterialTheme.colorScheme.primary,
@@ -64,10 +73,14 @@ fun Greeting(name: String) {
             Column(
                 modifier = Modifier
                     .weight(1f)
-                    .padding(bottom = extraPadding)
+                    .padding(bottom = extraPadding.coerceAtLeast(0.dp))
             ) {
                 Text(text = "Hello,")
-                Text(text = name)
+                Text(
+                    text = name, style = MaterialTheme.typography.headlineMedium.copy(
+                        fontWeight = FontWeight.ExtraBold
+                    )
+                )
             }
             ElevatedButton(
                 onClick = { expanded = !expanded }
@@ -114,7 +127,21 @@ fun OnboardingScreen(
 @Preview(showBackground = true, widthDp = 320)
 @Composable
 fun DefaultPreview() {
-    JetPackComposeBasicTheme {
+    JetPackComposeBasicTheme(dynamicColor = false) {
+        Greetings()
+    }
+}
+
+@Preview(
+    showBackground = true,
+    widthDp = 320,
+    uiMode = UI_MODE_NIGHT_YES,
+    name = "Dark"
+)
+@Preview(showBackground = true, widthDp = 320)
+@Composable
+fun DefaultPreviewDark() {
+    JetPackComposeBasicTheme(dynamicColor = false) {
         Greetings()
     }
 }
