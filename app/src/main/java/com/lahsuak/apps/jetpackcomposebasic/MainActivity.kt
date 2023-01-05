5bb74fcee454f17b46b7e925a3622ba85f812397
Kaushal-Vasava <kaushal.vasava@algozen.in>
2023-01-05 11:32:45 +0530

    Add Composable function for greeting
    - Info of composable function

---
+++ app/src/main/java/com/lahsuak/apps/jetpackcomposebasic/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.lahsuak.apps.jetpackcomposebasic
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.lahsuak.apps.jetpackcomposebasic.ui.theme.JetPackComposeBasicTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            JetPackComposeBasicTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+/***
+Composable functions :
+A composable function is a regular function annotated with @Composable.
+This enables your function to call other @Composable functions within it.
+You can see how the Greeting function is marked as @Composable.
+This function will produce a piece of UI hierarchy displaying the given input,
+String. Text is a composable function provided by the library.
+***/
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    JetPackComposeBasicTheme {
+        Greeting("Android")
+    }
+}
