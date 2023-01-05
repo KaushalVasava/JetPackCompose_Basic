ec794aff20146d65e82c286887bfbba6ac10bf00
Kaushal-Vasava <kaushal.vasava@algozen.in>
2023-01-05 12:04:16 +0530

    Reused composable function

--- app/src/main/java/com/lahsuak/apps/jetpackcomposebasic/MainActivity.kt
+++ app/src/main/java/com/lahsuak/apps/jetpackcomposebasic/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -18,14 +19,19 @@ class MainActivity : ComponentActivity() {
         setContent {
             JetPackComposeBasicTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+                MyApp(modifier = Modifier.fillMaxSize())
             }
         }
+    }
+}
+
+@Composable
+fun MyApp(modifier: Modifier = Modifier) {
+    Surface(
+        modifier = modifier,
+        color = MaterialTheme.colorScheme.background
+    ) {
+        Greeting("Android")
     }
 }
 
@@ -48,6 +54,6 @@ fun Greeting(name: String) {
 @Composable
 fun DefaultPreview() {
     JetPackComposeBasicTheme {
-        Greeting("Android")
+        MyApp()
     }
 }
