eaa6d676ab7f1785d0c07a6ac576664f77b58183
KaushalVasava <vasavakaushal333@gmail.com>
2023-08-30 13:16:53 +0530

    Add Navigation back with result functionality

--- app/src/main/java/com/lahsuak/apps/jetpackcomposebasic/MainActivity.kt
+++ app/src/main/java/com/lahsuak/apps/jetpackcomposebasic/MainActivity.kt
@@ -1,15 +1,34 @@
 package com.lahsuak.apps.jetpackcomposebasic
 
 import android.os.Bundle
+import android.widget.Space
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.lahsuak.apps.jetpackcomposebasic.ui.theme.JetPackComposeBasicTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,29 +41,63 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    val navController = rememberNavController()
+                    NavHost(
+                        navController = navController,
+                        startDestination = "firstscreen"
+                    ) {
+                        composable("firstscreen") {
+                            FirstScreen(navController)
+                        }
+                        composable("secondscreen") {
+                            SecondScreen(navController)
+                        }
+                    }
                 }
             }
         }
     }
 }
-/***
-Composable functions :
-A composable function is a regular function annotated with @Composable.
-This enables your function to call other @Composable functions within it.
-You can see how the Greeting function is marked as @Composable.
-This function will produce a piece of UI hierarchy displaying the given input,
-String. Text is a composable function provided by the library.
-***/
+
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun FirstScreen(navController: NavController) {
+    val msg = navController.currentBackStackEntry?.savedStateHandle?.get<String>("msg")
+    Column(
+        Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Button(onClick = { navController.navigate("secondscreen") }) {
+            Text("Go to next screen")
+        }
+        Spacer(modifier = Modifier.height(8.dp))
+        msg?.let {
+            Text(it)
+        }
+    }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    JetPackComposeBasicTheme {
-        Greeting("Android")
+fun SecondScreen(navController: NavController) {
+    var text by remember {
+        mutableStateOf("")
+    }
+    Column(
+        Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        TextField(
+            value = text, onValueChange = { text = it },
+            placeholder = {
+                Text("Enter text", color = Color.Gray)
+            })
+        Spacer(Modifier.height(8.dp))
+        Button(onClick = {
+            navController.previousBackStackEntry?.savedStateHandle?.set("msg", text)
+            navController.popBackStack()
+        }) {
+            Text(text = "Submit")
+        }
     }
 }
